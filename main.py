c8f1eb2cdb0b54b313c953b6d435519e4a8b95a4
Jakub Dublewicz <dublewiczjakub@gmail.com>
2025-11-12 21:05:05 +0100

    Add printing for shooping info

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ zakupy = {
      "piekarnia": ["chleb", "bułki", "pączek"],
      "warzywniak": ["marchew", "seler", "rukola"]
  }
+for shop, products in zakupy.items():
+    print(f"Idę do {shop} i kupuję tam {', '.join(products)}.")
