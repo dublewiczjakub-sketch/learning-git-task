8b0d19bec1d3ff883f16fe227d65560f2aaa8970
Jakub Dublewicz <dublewiczjakub@gmail.com>
2025-11-16 10:41:06 +0100

    Niespodzianka

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-# Program: lista zakupów
-zakupy = {
-     "piekarnia": ["chleb", "bułki", "pączek", "zapiekanka"],
-     "warzywniak": ["marchew", "seler", "rukola","pomidor", "ziemniak"],
-     "kosmetyczny": ["szampon", "żel"]
- }
-total_products = 0
-
-for shop, products in zakupy.items():
-    shop_cap = shop.capitalize()
-    products_cap = [product.capitalize() for product in products]
-    print(f"Idę do {shop_cap} i kupuję tam {', '.join(products_cap)}.")
-    total_products += len(products)
-
-print(f"\nW sumie kupuję {total_products} produktów.")
+# Pozdrawiam swojego Mentora Patryka Tokarza. :)
